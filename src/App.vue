f0795d42c4e6eb06035656bb9baeb64c3fda3d72
Delia Sanchez Juarez <w@3.local>
2026-02-28 23:52:57 -0600

    Text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,111 @@
 <template>
-  <div>
-    Hello, Vue!!
-  </div>
+  <h1>{{ message }}</h1>
+
+  <hr/>
+
+  <h1>number: {{ number }}</h1>
+
+  <hr/>
+  
+  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
+  
+  <hr />
+  
+  <h1>number * 2 = :{{ number *2 }}</h1>
+  
+  <hr />
+  
+  <h1>
+    {{ number > 150? 'number is greater than 150':'number is less than 150'}}
+    </h1>
+  
+  <hr />
+  
+  <h1 v-text="number"></h1>
+  
+  <hr />
+  
+  <h1>{{ harry }}</h1>
+  <h1>{{ harry.name }}</h1>
+  
+  <hr>
+  
+  <h1>{{ hogwartsWizards }}</h1>
+  <h1>{{ hogwartsWizards[0] }}</h1>
+  
+  <hr>
+  
+  <h1>{{ rawHTML }}</h1>
+  <h1 v-text="rawHTML"></h1>
+  <h1 v-html="rawHTML"></h1>
 </template>
 
 <script setup>
+  let message = "Hello, Vue!!"
+  let number = 50
 
+  function doubleNum(num) {
+    return num * 2
+  }
+
+  let harry = {
+    id: 1001,
+    name: 'Harry Potter',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Phoenix feather',
+      wood: 'Holly'
+    }
+  }
+
+  const hogwartsWizards = [
+    {
+      id: 1001,
+      name: 'Harry Potter',
+      house: 'Gryffindor',
+      age: 17,
+      wand: {
+        core: 'Phoenix feather',
+        wood: 'Holly'
+      }
+    },
+
+    {
+      id: 1002,
+      name: 'Hermoine Granger',
+      house: 'Gryffindor',
+      age: 17,
+      wand: {
+        core: 'Dragon heartstring',
+        wood: 'Vine'
+      }
+    },
+
+    {
+      id: 1003,
+      name: 'Ron Weasley',
+      house: 'Gryffindor',
+      age: 17,
+      wand: {
+        core: 'Unicorn hair',
+        wood: 'Willow'
+      }
+    },
+
+    {
+      id: 1004,
+      name: 'Drago Malfoy',
+      house: 'Slytherin',
+      age: 17,
+      wand: {
+        core: 'Dragon heartstring',
+        wood: 'Hawthorn'
+      }
+    }
+  ]
+
+  let rawHTML = '<span style="color: red">This should be red.</span>'
 </script>
 
 <style lang="scss" scoped>
