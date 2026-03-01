ff395e6daa540a9a11048418cba5592e7158857a
Delia Sanchez Juarez <w@3.local>
2026-03-01 14:11:12 -0600

    Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    Hello, Vue!!
-  </div>
+  <h1>{{ message }}</h1>
+  <p>
+    What has keys but can't open locks, space but no room,
+    and can't go outside? What am I?
+  </p>
+
+  <button 
+    style="width: 200px"
+    @click="showAnswer = !showAnswer"
+    >{{!showAnswer ? 'Show me the answer!' : 'Hide the answer!'}}
+  </button>
+
+  <div v-if="showAnswer">A keyboard!</div>
+  <div v-show="showAnswer">A keyboard!</div>
+
+
+  <div v-if="Math.random() > 0.5">Now you see me</div>
+  <div v-else>Now you don't</div>
 </template>
 
 <script setup>
+  import {ref} from 'vue'
 
+  let message = ref('Hello, v-if!!')
+
+  let showAnswer = ref(false)
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
