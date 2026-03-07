3ea34e0b40668dfd1d6606fa928e2ea92155c6ab
Delia Sanchez Juarez <w@3.local>
2026-03-07 17:41:28 -0600

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-    Hello, Vue!!
+  <h1>{{ message }}</h1>
+
+  <div class="card">
+    <h1>Scenarion 1L Watch a "ref(primitive value)"</h1>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by 1</button>
   </div>
 </template>
 
 <script setup>
+  import {ref} from 'vue'
+
+  let message = ref('Hello, Watchers!!')
+
+  let number = ref(1)
+
+  let stopWatch = watch(
+    number, 
+    (newValue, oldValue) => {
+      console.log(
+        'Watch a ref(primitive value): number changes',
+        newValue,
+        oldValue
+      )
+      if (newValue > 5) {
+        stopWatch()
+      }
+  },
+  {immediate: true}
+)
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+  .card {
+    background-color: purple;
+    color: white;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+  }
 </style>
