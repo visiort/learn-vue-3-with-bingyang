9da3b56d0fa5578bf1ab55cca9190bcc29242898
Delia Sanchez Juarez <w@3.local>
2026-03-11 19:34:07 -0500

    Components-Intro

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    Hello, Vue!!
+  <ButtonCounter></ButtonCounter>
+  <ButtonCounter></ButtonCounter>
+
+  <button-counter></button-counter>
+
+  <hr />
+
+  <div style="background-color: orange; padding: 5px;">
+    This is the root component.
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
   </div>
 </template>
 
 <script setup>
-
+  import ButtonCounter from './ButtonCounter.vue'
+  import ComponentA from '@/ComponentA.vue'
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
 </style>
---
+++ src/ButtonCounter.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="count++">You clicked me {{ count }} times!</button>
+</template>
+
+<script>
+    import {ref} from 'vue'
+
+    let count = ref(0)
+</script>
+
+<style scoped>
+    button {
+        background-color: #4f46e5;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        margin-top: 10px;
+        transition: all 0.2s;
+    }
+
+    button:hover {
+        background-color: #4f46e5;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0, 0.2);
+    }
+</style>
