295aa4e31a7654b5edc294b961c231eb8f6f4383
Delia Sanchez Juarez <w@3.local>
2026-03-13 20:16:32 -0500

    Intro to Slots

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    Hello, Vue!!
-  </div>
+  <AlertBox>
+    Invalid username or password. Please try again
+  </AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>
+      The request timed out. Please refresh the page and try again.
+    </h1>
+  </AlertBox>
 </template>
 
 <script setup>
-
+import AlertBox from './AlertBox.vue'
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
