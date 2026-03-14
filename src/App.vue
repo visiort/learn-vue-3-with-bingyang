ef99fca19cd38b007c3c515422330038f9942e7a
Delia Sanchez Juarez <w@3.local>
2026-03-13 20:13:04 -0500

    Provide and Inject

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    Hello, Vue!!
+  <h1>Message: {{ message }}</h1>
+  <div 
+    style="background-color: orange; 
+    padding-left: 10px">
+    <ComponentA></ComponentA>
   </div>
 </template>
 
 <script setup>
+import { ref, provide, readonly } from 'vue'
+import ComponentA from './ComponentA.vue'
 
+let message = ref('Hello, Provide/inject!')
+
+function updateMessage() {
+  message.value = 'Hello, Provide/inject! Updated.'
+}
+
+provide('msg', { message, updateMessage })
+
+let count = ref(0)
+
+provide('read-only-count', readonly(count))
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
 </style>
