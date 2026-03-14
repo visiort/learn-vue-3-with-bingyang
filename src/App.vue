e70faa2fca8a7d634f8cf80f9bceb1f0cfcc6a51
Delia Sanchez Juarez <w@3.local>
2026-03-13 20:20:35 -0500

    Named Slots

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    Hello, Vue!!
-  </div>
+  <BaseLayout>
+    <template #header>
+      <h1>
+        Here might be a page title.
+      </h1>
+    </template>
+
+    <template #main>
+      <p>
+        A paragraph in the main content area.
+      </p>
+      <p>
+        And another one.
+      </p>
+    </template>
+ 
+    <template #footer>
+      <p>
+        Here is some contact information.
+      </p>
+    </template>
+  </BaseLayout>
 </template>
 
 <script setup>
-
+import BaseLayout from './BaseLayout.vue'
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
 
 </style>
