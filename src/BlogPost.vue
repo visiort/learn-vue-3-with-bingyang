37f83ea1229cb7aaef29ebc8f002baa28fc5ed25
Delia Sanchez Juarez <w@3.local>
2026-03-13 20:06:07 -0500

    Components V model

---
+++ src/BlogPost.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="blog-post">
+    <p>
+
+      {{ message }}
+    </p>
+    <h2>{{ id }} - {{ blogPostTitle }}</h2>
+    <div>Modify the title:
+      <input type="text" v-model="blogPostTitle" />
+    </div>
+    <h4>{{ blogPostContent }}</h4>
+    <div>
+
+
+      Modify the content:
+      <input type="text" v-model="blogPostContent" />
+    </div>
+    <button @click="$emit('delete-blog-post', id)">Delete post</button>
+    <button @click="emitDeletePostEvent(id)">Delete post</button>
+  </div>
+</template>
+
+<script setup>
+
+
+import { ref } from 'vue'
+
+
+let message = ref('This is the BlogPost component.')
+defineProps(['id'])
+
+
+const emit = defineEmits(['delete-blog-post'])
+function emitDeletePostEvent(id)
+{
+  emit('delete-blog-post', id)
+}
+
+let blogPostTitle = defineModel('blogPostTitle')
+let blogPostContent = defineModel('blogPostContent') 
+</script>
+
+
+
+<style scoped>
+.blog-post {
+  background-color: aqua;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+input {
+  width: 100%;
+}
+</style>
